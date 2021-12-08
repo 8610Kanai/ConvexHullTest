be1b3ee1966c97b739bb776dee1cf870b3336214
8610kanai <r02.gm.2100147@da.iwasaki.ac.jp>
2021-12-08 10:37:26 +0900

    Update Point.hpp

--- Point.hpp
+++ Point.hpp
@@ -15,6 +15,16 @@ public:
 
 	void SetLocation(float x, float y, float z);
 
+	D3DXVECTOR3 GetLocation() const
+	{
+		return D3DXVECTOR3
+		(
+			this->worldMatrix._41,
+			this->worldMatrix._42,
+			this->worldMatrix._43
+		);
+	};
+
 	void Render();
 
 private:
